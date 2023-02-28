3e51bb44a0c3bbc6d4ed7f0a2b4e6c5b32816a80
Konrad Dulęba <konrad.duleba@miquido.com>
2023-02-28 23:27:37 +0100

    feat(menu): added menu

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -1,10 +1,35 @@
+import { navigateToUrl } from "single-spa";
+
+import "./colors.scss";
 import styles from "./navigation.module.scss";
+
+interface MenuItem {
+  path: string;
+  name: string;
+}
+
+const MENU_LIST: MenuItem[] = [
+  {
+    path: "/",
+    name: "Welcome",
+  },
+  {
+    path: "/bimbows-3-1",
+    name: "Bimbows 3.1",
+  },
+];
 
 const Navigation = () => {
   return (
-    <div className={styles.navigation}>
-      <p>Navigation</p>
-    </div>
+    <ul className={styles.menu}>
+      {MENU_LIST.map(({ path, name }: MenuItem) => (
+        <li key={path}>
+          <a href={path} onClick={navigateToUrl} className={styles.item}>
+            {name}
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 };
 
